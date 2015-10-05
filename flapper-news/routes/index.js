c1c4b2748f3f1697cb4e2449f10b4390bd25bc9d
kngschool <kng13@mail.ccsf.edu>
2015-10-05 20:19:33 +0000

    Adding parital code to post js and index js

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -1,9 +1,65 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+router.get('/posts', function(req, res, next) 
+{
+  Post.find(function(err, posts)
+  {
+    if(err){ return next(err); }
+    res.json(posts);
+  });
+});
+
+router.post('/posts', function(req, res, next) 
+{
+  var post = new Post(req.body);
+
+  post.save(function(err, post)
+  {
+    if(err){ return next(err); }
+    res.json(post);
+  });
+});
+
+router.post('/posts', function(req, res, next) 
+{
+  var post = new Post(req.body);
+  post.save(function(err, post)
+  {
+    if(err){ return next(err); }
+    res.json(post);
+  });
+});
+
+router.param('post', function(req, res, next, id) 
+{
+  var query = Post.findById(id);
+
+  query.exec(function (err, post)
+  {
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find the post')); }
+    req.post = post;
+    return next();
+  });
+});
+
+router.put('/posts/:post/upvote', function(req, res, next) 
+{
+  req.post.upvote(function(err, post)
+  {
+    if (err) { return next(err); }
+    res.json(post);
+  });
+});
